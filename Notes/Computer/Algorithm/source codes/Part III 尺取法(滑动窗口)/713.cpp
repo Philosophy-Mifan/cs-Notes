3a91d87d63dac2bf5de7952f1efe2485ee587f50
mifannnn <philosophy_mifan@outlook.com>
2026-01-15 18:06:48 +0800

    unfinished doc

---
+++ Notes/Computer/Algorithm/source codes/Part III 尺取法(滑动窗口)/713.cpp
@@ -0,0 +1,36 @@
+#pragma warning(disable:4996)
+#include<iostream>
+#include<cstdio>
+#include<vector>
+#include<limits.h>
+using namespace std;
+int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+	int res = INT_MAX;
+	int n = nums.size();
+	int left = 0, right = 0;
+	int sub = 1;	//乘积
+	while (right < n) {
+		sub *= nums[right];
+		while (left <= right && (sub / nums[left]) < k) {
+			sub /= nums[left];
+			left++;
+		}
+		if (sub < k) {
+			res = max(res, right - left + 1);
+		}
+	}
+
+}
+
+int main() {
+	int k = 0, n = 0, x = 0;
+	vector<int> nums;
+	scanf("%d %d", &k, &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &x);
+		nums.push_back(x);
+	}
+	int ans = numSubarrayProductLessThanK(nums, k);
+	printf("%d\n", ans);
+	return 0;
+}
